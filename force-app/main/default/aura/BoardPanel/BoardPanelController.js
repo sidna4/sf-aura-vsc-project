95bee365762ea8f9647645468d690de4c5b4d39b
Sidney Antommarchi <sidna4@yahoo.com>
2021-09-06 08:23:49 -0400

    Lecture 225

--- force-app/main/default/aura/BoardPanel/BoardPanelController.js
+++ force-app/main/default/aura/BoardPanel/BoardPanelController.js
@@ -1,5 +1,16 @@
 ({
-    myAction : function(component, event, helper) {
+    startGame: function (component, event, helper) {
+        // access combobox
+        let gameModeComboBox = component.find("gameMode");
 
+        // access the value of combobox
+        let selectedValue = gameModeComboBox.get("v.value");
+
+        // update selectedMode attribute
+        component.set("v.selectedMode", selectedValue);
+    },
+
+    reshuffleBoard: function (component, event, helper) {
+        console.log("Reshuffle board is called");
     }
-})
+});
